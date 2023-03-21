3a4ea5ce405473a4701cabb21bd091c5e58e23c9
kotros <nas.m@bk.ru>
2023-03-21 23:04:30 +0300

    задание 13

---
+++ case13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая ввыводит третью цифру заданного числа или сообщает, что
+// третьей цифры нет.
+
+Console.Clear();
+Console.Write(" Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int Length = num.ToString().Length;
+if(Length >= 3) {
+    while(num > 999){
+        num = num / 10;
+    }
+    int result = num % 10;
+    Console.WriteLine(" Это третья цифра " + result);
+}
+else
+{
+    Console.WriteLine(" третьей цифры нет ");
+}
+
