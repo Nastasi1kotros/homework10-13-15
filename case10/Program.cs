7f98bac61c371b81d022b8d48a6593fe498913bb
kotros <nas.m@bk.ru>
2023-03-21 22:49:27 +0300

    задание 10

---
+++ case10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает трехзначное число и на выходе показывает вторую цифру
+// этого числа.
+
+Console.Clear();
+Console.Write(" Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine());
+int Length = num.ToString().Length;
+if(Length == 3) {
+    int result = (num / 10) % 10;
+    Console.WriteLine(" Вторая цифра " + result);
+}
+else
+{
+    Console.WriteLine(" Это не трехзначное число ");
+}
